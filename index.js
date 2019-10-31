b57f98a738292cbcd24dda1cb70cc458a5a1a79d
alefecrz <alefecrz@gmail.com>
2019-10-31 16:26:20 -0300

    Finilizer api.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const banco = [];
+
+server.get('/',(request, response)=>{
+  return response.json(banco);
+});
+
+server.get('/lastInfo',(request, response)=>{
+  return response.json(banco[banco.length - 1]);
+});
+
+server.post('/addInfo',(request, response)=>{
+  var info = request.body;
+  banco.push(info);
+  return response.json(banco);
+})
+
+server.listen(3333, ()=>{
+  console.log('Listen in port 3333.')
+});
