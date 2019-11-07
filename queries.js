2ce862833aa3cea97e08094b566b062307746568
Carlos H <riquinhuw_lass@hotmail.com>
2019-11-07 17:55:02 -0300

    Verdade

--- queries.js
+++ queries.js
@@ -31,7 +31,7 @@ const getUsers = (request, response) => {
 
   const createUser = (request, response) => {
     const { name, email } = request.body
-  
+    console.log(request.body)
     pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
       if (error) {
         throw error
